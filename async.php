fe9a88dc819c77cc4e56f3d6ca487547db293ac0
haseebqamar <haseebqamar92@gmail.com>
2018-09-30 01:14:58 +0500

    added asyc with multiple parameters

--- async.php
+++ async.php
@@ -2,36 +2,56 @@
 
 include 'connection.php';
  $var = $_GET['q'];
- $sql = "SELECT * FROM gta3 WHERE c_type ='$var'  ";
+ $search = $_GET['query'];
+ echo $search;
+ if ($search =='category') {
+   $sql = "SELECT * FROM gta3 WHERE c_type LIKE '{$var}%'  ";
+ }elseif ($search == 'desc') {
+   $sql = "SELECT * FROM gta3 WHERE c_desc LIKE '%{$var}%'  ";
+ }
+
  $result = $conn->query($sql) or die($conn->error);
  echo "<table id='mytable'>
  <tr id='desc'>
  <th>CHEAT DESCRIPTION</th>
  <th>CHEAT CODE</th>
  <th>CHEAT CATEGORY</th>
+ <th>GAME</th>
 
  </tr>"
 
  ;
- while($row = $result->fetch_assoc()) {
+ if ($var != '') {
+   $rowcount=mysqli_num_rows($result);
+  if ($rowcount > 0) {
+    while($row = $result->fetch_assoc()) {
 
-     echo "
+        echo "
 
-     <tr>
+        <tr>
 
-     <td>".$row['c_desc']."</td>
-     <td>".$row['c_code']."</td>
-     <td>".$row['c_type']."</td>
-
-
-
-     </tr>
+        <td>".$row['c_desc']."</td>
+        <td>".$row['c_code']."</td>
+        <td>".$row['c_type']."</td>
+        <td>".$row['game']."</td>
 
 
 
-     ";
+        </tr>
 
+
+
+        ";
+
+    }
+  }else{
+    // echo "0asda";
+  }
+
+
+  // echo $rowcount;
+ }else {
+   echo "empty";
  }
 
-echo '';
  ?>
